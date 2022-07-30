c77743dbf7210008b9b1cf10a4b3c6987cb1be86
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-30 14:24:42 -0500

    update api user dto

--- HotelListing.API/Models/User/ApiUserDto.cs
+++ HotelListing.API/Models/User/ApiUserDto.cs
@@ -2,16 +2,10 @@
 
 namespace HotelListing.API.Models.User;
 
-public class ApiUserDto
+public class ApiUserDto : LoginDto
 {
     [Required]
     public string FirstName { get; set; }
     [Required]
     public string LastName { get; set; }
-    [Required]
-    [EmailAddress]
-    public string Email { get; set; }
-    [Required]
-    [StringLength(25, ErrorMessage = "Your password must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
-    public string Password { get; set; }
 }
