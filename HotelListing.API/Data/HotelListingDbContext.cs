8cd284cfe1bd55be8f8b79d06fbf69d8f13f6fd2
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-30 12:42:18 -0500

    separate country and hotel data to configuration files add config

--- HotelListing.API/Data/HotelListingDbContext.cs
+++ HotelListing.API/Data/HotelListingDbContext.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+﻿using HotelListing.API.Data.Configurations;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace HotelListing.API.Data;
@@ -15,50 +16,8 @@ public class HotelListingDbContext : IdentityDbContext<ApiUser>
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
-        modelBuilder.Entity<Country>().HasData(
-            new Country
-            {
-                Id = 1,
-                Name = "Ireland",
-                ShortName = "IE"
-            },
-            new Country
-            {
-                Id = 2,
-                Name = "United Kingdom",
-                ShortName = "UK"
-            },
-            new Country
-            {
-                Id = 3,
-                Name = "Scotland",
-                ShortName = "SC"
-            });
-        modelBuilder.Entity<Hotel>().HasData(
-            new Hotel
-            {
-                Id = 1,
-                Name = "Brazen Head",
-                CountryId = 1,
-                Address = "Dublin",
-                Rating = 5
-            },
-            new Hotel
-            {
-                Id = 2,
-                Name = "King's Head",
-                CountryId = 2,
-                Address = "London",
-                Rating = 4
-            },
-            new Hotel
-            {
-                Id = 3,
-                Name = "Edinburgh Castle Hotel",
-                CountryId = 3,
-                Address = "Edinburgh",
-                Rating = 4.5
-            });
+        modelBuilder.ApplyConfiguration(new RoleConfiguration());
+        modelBuilder.ApplyConfiguration(new CountryConfiguration());
+        modelBuilder.ApplyConfiguration(new HotelConfiguration());
     }
-    
 }
