8bd48504759fee056906779b37c5555152adb97c
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-30 12:40:25 -0500

    add country configuration class

---
+++ HotelListing.API/Data/Configurations/CountryConfiguration.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.API.Data.Configurations;
+
+public class CountryConfiguration : IEntityTypeConfiguration<Country>
+{
+    public void Configure(EntityTypeBuilder<Country> builder)
+    {
+        builder.HasData(
+            new Country
+            {
+                Id = 1,
+                Name = "Ireland",
+                ShortName = "IE"
+            },
+            new Country
+            {
+                Id = 2,
+                Name = "United Kingdom",
+                ShortName = "UK"
+            },
+            new Country
+            {
+                Id = 3,
+                Name = "Scotland",
+                ShortName = "SC"
+            });
+    }
+}
