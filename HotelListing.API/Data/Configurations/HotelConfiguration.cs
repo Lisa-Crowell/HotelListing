3d5d5a6a8ddcb5d8de7fd2431f2eb78aa830fe03
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-30 12:40:43 -0500

    add hotel configuration class

---
+++ HotelListing.API/Data/Configurations/HotelConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace HotelListing.API.Data.Configurations;
+
+public class HotelConfiguration : IEntityTypeConfiguration<Hotel>
+{
+    public void Configure(EntityTypeBuilder<Hotel> builder)
+    {
+        builder.HasData(
+            new Hotel
+            {
+                Id = 1,
+                Name = "Brazen Head",
+                CountryId = 1,
+                Address = "Dublin",
+                Rating = 5
+            },
+            new Hotel
+            {
+                Id = 2,
+                Name = "King's Head",
+                CountryId = 2,
+                Address = "London",
+                Rating = 4
+            },
+            new Hotel
+            {
+                Id = 3,
+                Name = "Edinburgh Castle Hotel",
+                CountryId = 3,
+                Address = "Edinburgh",
+                Rating = 4.5
+            });
+    }
+}
