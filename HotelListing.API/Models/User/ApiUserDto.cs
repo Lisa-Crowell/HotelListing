1860d6e01b4429b164ccc5766a9d7ce13e5bab9f
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-30 12:41:09 -0500

    add api user dto

---
+++ HotelListing.API/Models/User/ApiUserDto.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.Models.User;
+
+public class ApiUserDto
+{
+    [Required]
+    public string FirstName { get; set; }
+    [Required]
+    public string LastName { get; set; }
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [StringLength(25, ErrorMessage = "Your password must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    public string Password { get; set; }
+}
