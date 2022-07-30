135ce41bf2e7ed40c01ac6e9a400bccc0beec7b8
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-30 12:42:42 -0500

    update mapping config

--- HotelListing.API/Configurations/MapperConfig.cs
+++ HotelListing.API/Configurations/MapperConfig.cs
@@ -2,6 +2,7 @@
 using HotelListing.API.Data;
 using HotelListing.API.Models.Country;
 using HotelListing.API.Models.Hotel;
+using HotelListing.API.Models.User;
 
 namespace HotelListing.API.Configurations;
 
@@ -16,5 +17,7 @@ public class MapperConfig : Profile
         
         CreateMap<Hotel, HotelDto>().ReverseMap();
         CreateMap<Hotel, CreateHotelDto>().ReverseMap();
+
+        CreateMap<ApiUser, ApiUserDto>().ReverseMap();
     }
 }
