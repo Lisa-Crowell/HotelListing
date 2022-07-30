7388883c7f256cbf9a60df92e69ff773c7c485fa
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-30 14:24:06 -0500

    add login dto class

---
+++ HotelListing.API/Models/User/LoginDto.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace HotelListing.API.Models.User;
+
+public class LoginDto
+{
+    [Required]
+    [EmailAddress]
+    public string Email { get; set; }
+    [Required]
+    [StringLength(25, ErrorMessage = "Your password must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
+    public string Password { get; set; }
+}
