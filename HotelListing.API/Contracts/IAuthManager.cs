2d43f8f81c83c5dcb14a901116b2739b9bf8ab4a
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-30 12:39:34 -0500

    add auth manager interface

---
+++ HotelListing.API/Contracts/IAuthManager.cs
@@ -0,0 +1,9 @@
+﻿using HotelListing.API.Models.User;
+using Microsoft.AspNetCore.Identity;
+
+namespace HotelListing.API.Contracts;
+
+public interface IAuthManager
+{
+    Task<IEnumerable<IdentityError>> RegisterUser(ApiUserDto userDto);
+}
