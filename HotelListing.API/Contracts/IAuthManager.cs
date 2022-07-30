a233c1630284888e6914a63e8adaae0f251c4137
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-30 16:19:22 -0500

    add token

--- HotelListing.API/Contracts/IAuthManager.cs
+++ HotelListing.API/Contracts/IAuthManager.cs
@@ -7,4 +7,6 @@ public interface IAuthManager
 {
     Task<IEnumerable<IdentityError>> RegisterUser(ApiUserDto userDto);
     Task<AuthResponseDto> Login(LoginDto loginDto);
+    Task<string> CreateRefreshToken();
+    Task<AuthResponseDto> VerifyRefreshToken(AuthResponseDto request);
 }
