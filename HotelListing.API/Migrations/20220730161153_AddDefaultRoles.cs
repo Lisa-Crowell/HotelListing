b9eb0d8a1626abe08e8f1e559070a1660be93ff2
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-07-30 12:43:00 -0500

    add migrations for roles

---
+++ HotelListing.API/Migrations/20220730161153_AddDefaultRoles.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelListing.API.Migrations
+{
+    public partial class AddDefaultRoles : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "36e31a28-067a-4074-81eb-8c37bb729765", "c62685b5-bde8-466f-a3e1-31f672bb9a54", "Admin", "ADMIN" });
+
+            migrationBuilder.InsertData(
+                table: "AspNetRoles",
+                columns: new[] { "Id", "ConcurrencyStamp", "Name", "NormalizedName" },
+                values: new object[] { "9a9c1da0-fac5-4623-ab0c-791ab5511173", "b80ca555-b86c-4aab-8810-71941071135b", "User", "USER" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "36e31a28-067a-4074-81eb-8c37bb729765");
+
+            migrationBuilder.DeleteData(
+                table: "AspNetRoles",
+                keyColumn: "Id",
+                keyValue: "9a9c1da0-fac5-4623-ab0c-791ab5511173");
+        }
+    }
+}
