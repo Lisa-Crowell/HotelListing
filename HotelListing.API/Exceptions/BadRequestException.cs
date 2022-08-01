1e49a468276c37e5b5019af4ca685e1b363818e7
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-08-01 15:59:51 -0500

    add bad request exception

---
+++ HotelListing.API/Exceptions/BadRequestException.cs
@@ -0,0 +1,9 @@
+﻿namespace HotelListing.API.Exceptions;
+
+public class BadRequestException : ApplicationException
+{
+    public BadRequestException(string message) : base(message)
+    {
+        
+    }
+}
