2226f83d62abfb26347100fd91605c678c139bf8
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-08-02 13:26:42 -0500

    add field paged result

--- HotelListing.API/Contracts/IGenericRepository.cs
+++ HotelListing.API/Contracts/IGenericRepository.cs
@@ -1,9 +1,12 @@
-﻿namespace HotelListing.API.Contracts;
+﻿using HotelListing.API.QueryParameters;
+
+namespace HotelListing.API.Contracts;
 
 public interface IGenericRepository<T> where T : class
 {
     Task<T> GetAsync(int? id);
     Task<List<T>> GetAllAsync();
+    Task<PagedResult<TResult>> GetAllAsync<TResult>(QueryParameters.QueryParameters queryParameters);
     Task<T> AddAsync(T entity);
     Task UpdateAsync(T entity);
     Task DeleteAsync(int id);
