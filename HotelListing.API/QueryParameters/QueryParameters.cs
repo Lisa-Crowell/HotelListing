b2efe82efc9ec10561bc3d43ca9cf7808ec4fcd0
Lisa-Crowell <lisa.l.crowell@gmail.com>
2022-08-02 13:24:56 -0500

    add query parameters class

---
+++ HotelListing.API/QueryParameters/QueryParameters.cs
@@ -0,0 +1,15 @@
+﻿namespace HotelListing.API.QueryParameters;
+
+public class QueryParameters
+{
+    private int _pageSize = 15;
+    public int StartIndex { get; set; } = 0;
+    public int PageNumber { get; set; } = 1;
+    public int PageSize
+    {
+        get => _pageSize;
+        set => _pageSize = (value > 100) ? 100 : value;
+    }
+}
+
+
